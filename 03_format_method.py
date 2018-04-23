7c9319322f61fd2107a86feb5eec941ffb96e832
Uthaeus <romanlavery@gmail.com>
2018-04-22 22:00:06 -0600

    Format method

---
+++ 03_format_method.py
@@ -0,0 +1,9 @@
+name = 'Kristine'
+age = 12
+product = 'Python eLearning course'
+
+# greeting = "Hi {0}, you are listed as {1} years old and you have purchased: {2}...".format(name, age, product)
+
+greeting = f"Hi {name}, you are listed as {age} years old and you have purchased: {product}"
+
+print(greeting)
