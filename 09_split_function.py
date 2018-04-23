0f2a3d7dfb500323ba396532225567ba58de1460
Uthaeus <romanlavery@gmail.com>
2018-04-23 09:36:34 -0600

    Guide to split function

---
+++ 09_split_function.py
@@ -0,0 +1,9 @@
+tags = 'python,coding,programming,development'
+
+list_of_tags = tags.split(',')
+
+print(list_of_tags) # => ['python', 'coding', 'programming', 'development']
+
+list_of_tags = tags.split()
+
+print(list_of_tags) # => ['python,coding,programming,development']
