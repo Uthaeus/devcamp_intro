ac1ea9d7d477637906a0415018d3b24d5d558902
Uthaeus <romanlavery@gmail.com>
2018-04-22 21:49:04 -0600

    Modern string interpolation

--- 01_string_portions.py
+++ 01_string_portions.py
@@ -1,1 +1,17 @@
 starter_sentence = 'The quick brown fox jumped'
+
+first = starter_sentence[0]
+second = starter_sentence[1]
+third = starter_sentence[2]
+
+new_sentence = first + second + third
+
+print(new_sentence)
+
+first_word = starter_sentence[0:3]
+
+print(first_word)
+
+second_sentence = 'Thy' + starter_sentence[3:]
+
+print(second_sentence)
